cae3a5492046521fa0176a3fb9e22cbcc8e40f1c
Nate McGrew <ncmcgrew@familysearch.org>
2017-05-10 12:58:44 -0600

    Add project to git
     - I need to share the code with a proctor to  pass off the badge

---
+++ foobar/src/foo/Log4jHelloWorld.java
@@ -0,0 +1,19 @@
+package foo;
+
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class Log4jHelloWorld {
+
+    static final Logger logger = Logger.getLogger(Log4jHelloWorld.class);
+
+    public static void main(String[] args)
+    {
+        //Configure logger
+//        BasicConfigurator.configure();
+        logger.setLevel(Level.DEBUG);
+        logger.debug("Hello World!");
+    }
+}
